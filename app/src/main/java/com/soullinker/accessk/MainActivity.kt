29c404e4c9bfbe2a45fabe602a66ee0c29b11ede
Soullinker47 <linkersoul77@gmail.com>
2025-10-04 21:40:15 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/soullinker/accessk/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.soullinker.accessk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Scaffold { Text("Access-k", style = MaterialTheme.typography.headlineMedium) }
+        }
+    }
+}
